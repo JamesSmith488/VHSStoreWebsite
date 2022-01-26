fbd66088c4ba17c86bdb9b7c749491130e4fdea4
James Smith <james.lance.smith@hotmail.co.uk>
2022-01-26 12:35:08 +0000

    Initial commit

---
+++ src/test/java/com/sparta/vhsstorewebsite/VhsStoreWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.vhsstorewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhsStoreWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
