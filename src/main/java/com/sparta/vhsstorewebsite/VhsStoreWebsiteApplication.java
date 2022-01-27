c69ae3ff86cba0d464385e3f090234a3cef37880
resh <reshmansiva96@gmail.com>
2022-01-27 16:29:12 +0000

    uncommenting section to test login feature

--- src/main/java/com/sparta/vhsstorewebsite/VhsStoreWebsiteApplication.java
+++ src/main/java/com/sparta/vhsstorewebsite/VhsStoreWebsiteApplication.java
@@ -6,25 +6,24 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class VhsStoreWebsiteApplication {
-//    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     public static void main(String[] args) {
         SpringApplication.run(VhsStoreWebsiteApplication.class, args);
     }
 
 
-//    @Bean
-//    public CommandLineRunner demo(UserRepository userRepository){
-//        return args -> {
-//            userRepository.save(new UserEntity(1, "Alexander", "Jarrett" ,  "NO",
-//                    "skegness",encoder.encode("Password"), "STAFF"));
-//            userRepository.save(new UserEntity(2, "John", "Johnathan" ,  "YES",
-//                    "skegness",encoder.encode("Passverd"), "CUSTOMER"));
-//
-//        };
-//    }
+    @Bean
+    public CommandLineRunner demo(UserRepository userRepository){
+        return args -> {
+            userRepository.save(new UserEntity(1, "jack", "jackathon" ,  "no",
+                    "skegness",encoder.encode("password"), "STAFF"));
+            userRepository.save(new UserEntity(2, "john", "johnathan" ,  "yes",
+                    "skegness",encoder.encode("passwordjohn"), "CUSTOMER"));
+        };
+    }
 }
