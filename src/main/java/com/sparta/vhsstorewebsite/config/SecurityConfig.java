3479008610c5fdf84403499b406f8dcb1f11ac34
resh <reshmansiva96@gmail.com>
2022-01-27 16:26:40 +0000

    uncommenting securityConfig

--- src/main/java/com/sparta/vhsstorewebsite/config/SecurityConfig.java
+++ src/main/java/com/sparta/vhsstorewebsite/config/SecurityConfig.java
@@ -1,46 +1,61 @@
-//package com.sparta.vhsstorewebsite.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//
-//import javax.sql.DataSource;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    private final DataSource dataSource;
-//    private final BCryptPasswordEncoder encoder;
-//
-//    @Autowired
-//    public SecurityConfig(DataSource dataSource, BCryptPasswordEncoder encoder) {
-//        this.dataSource = dataSource;
-//        this.encoder = encoder;
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//
-//        auth.jdbcAuthentication()
-//                .dataSource(dataSource)
-//                .usersByUsernameQuery("select email,password from user_entity where email=?")
-//                .authoritiesByUsernameQuery("select email,role from user_entity where email=?")
-//                .passwordEncoder(encoder);
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests().antMatchers("/show-customers").hasAnyAuthority("ADMIN")
-//                .antMatchers("/css/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and().formLogin().loginPage("/login").permitAll()
-//                .defaultSuccessUrl("/", true).permitAll()
-//                .and().exceptionHandling().accessDeniedPage("/access-denied")
-//                .and().logout();
-//    }
-//}
+package com.sparta.vhsstorewebsite.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+    private final BCryptPasswordEncoder encoder;
+
+    @Autowired
+    public SecurityConfig(DataSource dataSource, BCryptPasswordEncoder encoder) {
+        this.dataSource = dataSource;
+        this.encoder = encoder;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select email,password from user_entity where email=?")
+                .authoritiesByUsernameQuery("select email,role from user_entity where email=?")
+                .passwordEncoder(encoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+
+                .antMatchers("/", "/index").permitAll()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .failureUrl("/error")
+                .usernameParameter("email")
+                .defaultSuccessUrl("/admin", true)
+                .permitAll()
+                .and()
+                .exceptionHandling().accessDeniedPage("/error")  // might need to be changed
+                .and()
+                .logout()
+//                .logoutUrl("login")
+//                .deleteCookies("remember-me")
+//                .permitAll()
+//                .and()
+//                .rememberMe()
+                ;
+    }
+}
