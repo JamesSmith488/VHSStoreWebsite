50addd1586c6c882fef00fbc4515120d20d8b5cb
Alexander Jarrett <alexjarrett400@gmail.com>
2022-01-27 11:57:39 +0000

    Added user entities for logging in. Created and commented method to create 2 initial users.

--- src/main/java/com/sparta/vhsstorewebsite/VhsStoreWebsiteApplication.java
+++ src/main/java/com/sparta/vhsstorewebsite/VhsStoreWebsiteApplication.java
@@ -1,13 +1,30 @@
 package com.sparta.vhsstorewebsite;
 
+import com.sparta.vhsstorewebsite.entities.UserEntity;
+import com.sparta.vhsstorewebsite.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class VhsStoreWebsiteApplication {
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     public static void main(String[] args) {
         SpringApplication.run(VhsStoreWebsiteApplication.class, args);
     }
 
+
+//    @Bean
+//    public CommandLineRunner demo(UserRepository userRepository){
+//        return args -> {
+//            userRepository.save(new UserEntity(1, "Alexander", "Jarrett" ,  "NO",
+//                    "skegness",encoder.encode("Password"), "STAFF"));
+//            userRepository.save(new UserEntity(2, "John", "Johnathan" ,  "YES",
+//                    "skegness",encoder.encode("Passverd"), "CUSTOMER"));
+//
+//        };
+//    }
 }
