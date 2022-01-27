d3e05bbe44e5f75016bc3d3d7ddcc42abdbdf7b5
Alexander Jarrett <alexjarrett400@gmail.com>
2022-01-27 16:31:54 +0000

    Adding repo annotation

--- src/main/java/com/sparta/vhsstorewebsite/repositories/WaitingUserRepository.java
+++ src/main/java/com/sparta/vhsstorewebsite/repositories/WaitingUserRepository.java
@@ -1,6 +1,8 @@
 package com.sparta.vhsstorewebsite.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WaitingUserRepository extends JpaRepository {
 }
