e87b7c2028d725dc9bb9a6ec8bfc5b308eb09739
mthomas1331 <mthomas6298@outlook.com>
2022-01-28 12:29:10 +0000

    Added Actor Page and its corresponding basic functionality

--- src/main/java/com/sparta/vhsstorewebsite/repositories/ActorRepository.java
+++ src/main/java/com/sparta/vhsstorewebsite/repositories/ActorRepository.java
@@ -4,6 +4,11 @@ import com.sparta.vhsstorewebsite.entities.ActorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ActorRepository extends JpaRepository<ActorEntity, Integer> {
+
+    List<ActorEntity> findByFirstNameContainsAndLastNameContains(String firstName, String lastName);
+
 }
