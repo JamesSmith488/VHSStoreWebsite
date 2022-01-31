ed678c4f14e564bd4415b84b36932980a8d5a965
resh <reshmansiva96@gmail.com>
2022-01-31 16:00:29 +0000

    helper method

--- src/main/java/com/sparta/vhsstorewebsite/repositories/FilmCategoryRepository.java
+++ src/main/java/com/sparta/vhsstorewebsite/repositories/FilmCategoryRepository.java
@@ -1,9 +1,16 @@
 package com.sparta.vhsstorewebsite.repositories;
 
+import com.sparta.vhsstorewebsite.entities.CategoryEntity;
 import com.sparta.vhsstorewebsite.entities.FilmCategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FilmCategoryRepository extends JpaRepository<FilmCategoryEntity, Integer> {
+
+
+    FilmCategoryEntity findByFilmId(Integer id);
+
 }
